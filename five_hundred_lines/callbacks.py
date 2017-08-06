a0401894350960bbe467e895b6b2be0080136a6e
Ken Branco <kenneth_branco@bose.com>
2017-08-06 00:11:58 -0400

    functional callback link crawler

--- five_hundred_lines/callbacks.py
+++ five_hundred_lines/callbacks.py
@@ -1,12 +1,14 @@
+import re
 import socket
 from selectors import DefaultSelector, EVENT_WRITE, EVENT_READ
 
 
-urls_todo = set(['/'])
-seen_urls = set(['/'])
+urls_todo = set(['/feeds/lists.shtml'])
+seen_urls = set(['/feeds/lists.shtml'])
+url_link_map = {}
+url_new_link_map = {}
+
 selector = DefaultSelector()
-
-
 
 class Fetcher:
     def __init__(self, url):
@@ -15,16 +17,29 @@ class Fetcher:
         self.sock = None
 
     def parse_links(self):
-        # TODO: implement parse links
-        print('parse_links called')
-        return set(['/'])
+        parsed_links = set()
+        try:
+            for line in self.response.decode('utf-8').split('\r\n'):
+                if line.find('a href=') > 0:
+                    links_list = re.findall("<a\shref=\"((http|https):\/\/.+?)\"", line)
+                    for link_item in links_list:
+                        parsed_link, http_type = link_item
+                        if http_type == 'http':
+                            if parsed_link[-3::].lower() not in ['pdf', 'php', 'ocx']:
+                                parsed_links.add(parsed_link)
+                            # print("{}: {}".format(http_type, link))
+        except UnicodeDecodeError as decode_error:
+            return parsed_links
+
+        url_link_map[self.url] = parsed_links
+        return parsed_links
 
     # Method on Fetcher class.
     def fetch(self):
         self.sock = socket.socket()
         self.sock.setblocking(False)
         try:
-            self.sock.connect(('xkcd.com', 80))
+            self.sock.connect(('www.uen.org', 80))
         except BlockingIOError:
             pass
 
@@ -35,11 +50,11 @@ class Fetcher:
 
     # Method on Fetcher class.
     def connected(self, key, mask):
-        print('connected!')
+        print('connected: %s' % self.url)
         selector.unregister(key.fd)
         #TODO: get the request format right to return page
-        request = 'GET {} HTTP/1.0\r\nHost: xkcd.com\r\n\r\n'.format(self.url)
-        self.sock.send(request.encode('ascii'))
+        request = 'GET {} HTTP/1.1\r\nHost: www.uen.org\r\n\r\n'.format(self.url)
+        self.sock.send(request.encode('utf-8'))
 
         # Register the next callback.
         selector.register(key.fd,
@@ -49,27 +64,37 @@ class Fetcher:
     # Method on Fetcher class.
     def read_response(self, key, mask):
         global stopped
+        global url_link_map
+        global url_new_link_map
 
-        chunk = self.sock.recv(4096)  # 4k chunk size.
+        chunk = self.sock.recv(50000000)  # 4k chunk size.
         if chunk:
             self.response += chunk
         else:
+            print('read complete: %s' % self.url)
             selector.unregister(key.fd)  # Done reading.
+            # print(self.response)
             links = self.parse_links()
+            print('{} - links found: {}'.format(self.url, len(links)))
 
             # Python set-logic:
-            for link in links.difference(seen_urls):
+            new_links = links.difference(seen_urls)
+            url_new_link_map[self.url] = new_links
+            print('{} - new links: {}'.format(self.url, len(new_links)))
+            for link in new_links:
                 urls_todo.add(link)
-                Fetcher(link).fetch()  # <- New Fetcher.
 
+                Fetcher(link).fetch()  # <- New Fetcher.
             seen_urls.update(links)
             urls_todo.remove(self.url)
-            if not urls_todo:
+            print('urls seen %i --> urls_todo %i' % (len(seen_urls), len(urls_todo)))
+
+            if len(seen_urls) > 250 or len(urls_todo) == 0:
                 stopped = True
 
 
 # Begin fetching http://xkcd.com/353/
-fetcher = Fetcher('https://xkcd.com/353/')
+fetcher = Fetcher('/feeds/lists.shtml')
 fetcher.fetch()
 
 
@@ -80,8 +105,12 @@ def loop():
     while not stopped:
         events = selector.select()
         for event_key, event_mask in events:
-            print('EVENT CAUGHT: {%s}' % event_key.data.__func__)
+            # print('EVENT CAUGHT: {%s}' % event_key.data.__func__)
             callback = event_key.data
             callback(event_key, event_mask)
 
 loop()
+for url_key, link_values in url_new_link_map.items():
+    print('URL: %s' % url_key)
+    for link_value in link_values:
+        print('{:@>4}'.format(link_value))
