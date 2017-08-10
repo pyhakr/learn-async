395a42b269381546557d2e422f6ec09210cd0eed
Ken Branco <kenneth_branco@bose.com>
2017-08-09 22:09:05 -0400

    dependency injection example

---
+++ dependency_injection.py
@@ -0,0 +1,33 @@
+
+class Common():
+    def get_api(self):
+        return 'common api -->'
+
+class Speaker(Common):
+    def get_api(self):
+        api = super().get_api()
+        return '{} regular speaker api -->'.format(api)
+
+class Headset(Common):
+    def get_api(self):
+        api = super().get_api()
+        return '{} regular headset api -->'.format(api)
+
+class SpecialSpeaker(Common):
+    def get_api(self):
+        api = super().get_api()
+        return '{} special speaker api -->'.format(api)
+
+class SpecialHeadset(Common):
+    def get_api(self):
+        api = super().get_api()
+        return '{} special headset api -->'.format(api)
+
+class Dut(SpecialHeadset, Headset):
+    def __init__(self):
+        for c in Dut.__mro__:
+            print(c)
+
+
+d = Dut()
+print(d.get_api())
